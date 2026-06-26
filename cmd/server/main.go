09cea7c6397f7bb64a7e3cfa0774b87a2c8e5c2c
Rashid Gizatullin <rashidg@halykbank.kz>
2026-06-26 16:50:10 +0500

    task 04 is done

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,12 +6,13 @@ import (
 	"io"
 	"log"
 	"net"
+	"strings"
 	"sync"
 )
 
 type Server struct {
-	clients map[net.Conn]struct{}
-	mu sync.RWMutex
+	clients map[net.Conn]string
+	mu      sync.RWMutex
 }
 
 func main() {
@@ -21,8 +22,8 @@ func main() {
 	}
 	defer listener.Close()
 	fmt.Println("server is listening at port 8080...")
-	server := &Server{
-		clients: make(map[net.Conn]struct{}),
+	s := &Server{
+		clients: make(map[net.Conn]string),
 	}
 	for {
 		conn, err := listener.Accept()
@@ -30,37 +31,69 @@ func main() {
 			log.Printf("unable to accept connection: %v\n", err)
 			continue
 		}
-		server.mu.Lock()
-		server.clients[conn] = struct{}{}
-		server.mu.Unlock()
-		go handleConn(conn, server)
+		s.mu.Lock()
+		s.clients[conn] = ""
+		s.mu.Unlock()
+		go handleConn(conn, s)
 	}
 }
 
 func handleConn(conn net.Conn, s *Server) {
 	defer conn.Close()
 	log.Printf("[+] New connection: %s\n", conn.RemoteAddr().String())
+	fmt.Fprint(conn, "Enter your name: ")
 	reader := bufio.NewReader(conn)
+	userName := ""
 	for {
 		input, err := reader.ReadString('\n')
 		if err != nil {
 			if err == io.EOF {
 				s.mu.Lock()
+				name := s.clients[conn]
 				delete(s.clients, conn)
+				if name != "" {
+					for nextConn := range s.clients {
+						if s.clients[nextConn] != "" {
+							fmt.Fprintf(nextConn, "* %s left the chat\n", name)
+						}
+					}
+				}
 				s.mu.Unlock()
-				log.Printf("[-] Disconnected: %s", conn.RemoteAddr().String())
 				break
 			} else {
 				log.Printf("unable to read client input: %v", err)
 				break
 			}
 		}
-		s.mu.RLock()
-		for nextConn := range s.clients {
-			if nextConn != conn {
-				fmt.Fprintf(nextConn, "[%s]: %s", conn.RemoteAddr().String(), input)
+		if userName == "" {
+			if validateName(input) {
+				s.mu.Lock()
+				userName = strings.TrimSpace(input)
+				s.clients[conn] = userName
+				for nextConn := range s.clients {
+					if nextConn != conn && s.clients[nextConn] != "" {
+						fmt.Fprintf(nextConn, "* %s joined the chat\n", s.clients[conn])
+					}
+				}
+				s.mu.Unlock()
+			} else {
+				fmt.Fprintln(conn, "Name cannot be empty, longer than 20 characters, or contain any space characters.")
+				fmt.Fprint(conn, "Enter your name: ")
+				continue
 			}
+		} else {
+			s.mu.RLock()
+			for nextConn := range s.clients {
+				if nextConn != conn && s.clients[nextConn] != "" && strings.TrimSpace(input) != "" {
+					fmt.Fprintf(nextConn, "[%s]: %s", s.clients[conn], input)
+				}
+			}
+			s.mu.RUnlock()
 		}
-		s.mu.RUnlock()
 	}
+}
+
+func validateName(name string) bool {
+	trimmedName := strings.TrimSpace(name)
+	return trimmedName != "" && len(trimmedName) <= 20 && !strings.Contains(trimmedName, " ")
 }
