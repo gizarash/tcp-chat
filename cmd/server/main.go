4cf3ac3e0c338c7bb4f207529119fd9e71ff1121
Rashid Gizatullin <rashidg@halykbank.kz>
2026-06-26 09:54:01 +0500

    task 02 is done

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,39 +4,38 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"log"
 	"net"
 )
 
 func main() {
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
-		fmt.Printf("unable to start listener: %v\n", err)
-		return
+		log.Fatalf("unable to start listener: %v", err)
 	}
 	defer listener.Close()
 	fmt.Println("server is listening at port 8080...")
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Printf("unable to accept connection: %v\n", err)
-			return
+			log.Fatalf("unable to accept connection: %v\n", err)
 		}
-		handleConn(conn)
+		go handleConn(conn)
 	}
 }
 
 func handleConn(conn net.Conn) {
 	defer conn.Close()
-	fmt.Printf("[+] New connection: %s\n", conn.RemoteAddr().String())
+	log.Printf("[+] New connection: %s\n", conn.RemoteAddr().String())
 	reader := bufio.NewReader(conn)
 	for {
 		input, err := reader.ReadString('\n')
 		if err != nil {
 			if err == io.EOF {
-				fmt.Printf("[-] Disconnected: %s\n", conn.RemoteAddr().String())
+				log.Printf("[-] Disconnected: %s", conn.RemoteAddr().String())
 				break
 			} else {
-				fmt.Printf("unable to read client input: %v\n", err)
+				log.Printf("unable to read client input: %v", err)
 				break
 			}
 		}
